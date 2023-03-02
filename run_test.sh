8d460357874c8db1a322b2a546448bb7c43ba28e
Ritika Dhiman <35415933+ritika99@users.noreply.github.com>
2023-03-02 03:29:28 -0500

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-/usr/bin/pip3 --install --upgrade pip
+sudo apt-get update
+sudo apt-get install python3-pip
+pip3 --install --upgrade pip
 pip install -r requirements.txt
 python -m pytests test.py
 echo "DONE!"
